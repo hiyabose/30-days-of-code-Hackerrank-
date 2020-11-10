57283babd9ecc113876d1c87b05d7f5219043f70
DIKSHITA BASU <hiya.bose26@gmail.com>
2020-11-10 20:31:12 +0530

    Create spiralmatrix.cpp

---
+++ spiralmatrix.cpp
@@ -0,0 +1,110 @@
+/*Given a matrix mat[][] of size M*N. Traverse and print the matrix in spiral form.
+
+Input:
+The first line of the input contains a single integer T, denoting the number of test cases. Then T test cases follow. Each testcase has 2 lines. First line contains M and N respectively separated by a space. Second line contains M*N values separated by spaces.
+
+Output:
+Elements when travelled in Spiral form, will be displayed in a single line.
+
+Constraints:
+1 <= T <= 100
+2 <= M,N <= 10
+0 <= Ai <= 100
+
+Example:
+Input:
+2
+4 4
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+3 4
+1 2 3 4 5 6 7 8 9 10 11 12
+
+Output:
+1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+1 2 3 4 8 12 11 10 9 5 6 7
+*/
+
+
+
+// C++ Program to print a matrix spirally
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define R 3
+#define C 6
+ 
+void spiralPrint(int m, int n, int a[R][C])
+{
+    int i, k = 0, l = 0;
+ 
+    /* k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator
+    */
+ 
+    while (k < m && l < n) {
+        /* Print the first row from
+               the remaining rows */
+        for (i = l; i < n; ++i) {
+            cout << a[k][i] << " ";
+        }
+        k++;
+ 
+        /* Print the last column
+         from the remaining columns */
+        for (i = k; i < m; ++i) {
+            cout << a[i][n - 1] << " ";
+        }
+        n--;
+ 
+        /* Print the last row from
+                the remaining rows */
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                cout << a[m - 1][i] << " ";
+            }
+            m--;
+        }
+ 
+        /* Print the first column from
+                   the remaining columns */
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                cout << a[i][l] << " ";
+            }
+            l++;
+        }
+    }
+}
+ 
+/* Driver Code */
+int main()
+{
+    int a[R][C] = { { 1, 2, 3, 4, 5, 6 },
+                    { 7, 8, 9, 10, 11, 12 },
+                    { 13, 14, 15, 16, 17, 18 } };
+     
+      // Function Call
+    spiralPrint(R, C, a);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.
